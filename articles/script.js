63289719bbf21b553d922663da7b54b5c39025b6
raccess21 <rahulanand0021@outlook.com>
2024-03-29 20:40:44 +0530

    lang complete

--- articles/script.js
+++ articles/script.js
@@ -9,9 +9,13 @@ function createArticleLinks(eId, jsonObj) {
 
 function langSelector(eId, jsonObj) {
     const langs = jsonObj[localStorage.getItem("articleName")]["langs"];
-    const lang = localStorage.getItem("lang") || "en";
+    langList(eId, langs);
+}
+
+function langList(eId, langs, lang=false) {
+    lang = lang || localStorage.getItem("lang");
     for (let i=0; i<langs.length; i++) {
-        if (langs[i] === lang) {
+        if (langs[i] == lang) {
             [langs[0], langs[i]] = [langs[i], langs[0]]
             break;
         }
@@ -36,6 +40,7 @@ function createPara(eId, jsonObj) {
 
 function loadJson(path, eId=false, callback=false) {
     let xobj = new XMLHttpRequest();
+    console.log(path)
     xobj.open('GET', path, true);
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
@@ -49,7 +54,7 @@ function loadJson(path, eId=false, callback=false) {
 
 function changeSize(bulla, eId) {
     const article = document.getElementById(eId);
-    const currentFontSize = parseInt(article.style.fontSize);
+    const currentFontSize = parseInt(article.style.fontSize) || 16;
     const newFontSize = (bulla ? currentFontSize + 1 : currentFontSize - 1) + "px";
     article.style.fontSize = newFontSize;
     localStorage.setItem('fontSize', newFontSize);
