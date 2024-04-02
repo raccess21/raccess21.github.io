a1894fb1805a13bf42a35042bb86527e9ef5d9ef
raccess21 <rahulanand0021@outlook.com>
2024-04-02 11:03:57 +0530

    articles added

--- articles/articles/script.py
+++ articles/articles/script.py
@@ -9,16 +9,25 @@ def lane(lang, articleName):
 # reads all articles folder, scans for all lang.json files
 # updates lang list in articles.json file
 def updateLangList():
+    articles = {}
+
     with open("articles.json", "r") as fi:
         articlesData = json.loads(fi.read())
 
     for article in  glob("*/"):
-        articleName = article.split("/")[0]
-        articlesData[articleName]["langs"] = [lane(lang, articleName) for lang in glob(f"{article}/*")]
+        articleName = article.split("/")[0]         #get articleName
+        print(articleName)
+        articles[articleName] = {
+            "langs": [[lane(lang, articleName) for lang in glob(f"{article}/*")]],
+        }
+        tags = {}
+        if articleName in articlesData.keys():                         
+            tags = articlesData[articleName]["tags"]
+        articles[articleName]["tags"] = tags
     
     try:
         with open("articles.json", "w") as fo:
-            fo.write(json.dumps(articlesData, indent=2))
+            fo.write(json.dumps(articles, indent=2))
     except:
         print("Error writing file!")
 
