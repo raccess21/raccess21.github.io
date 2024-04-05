68755e41210bd359774a07cf78708951621c9509
raccess21 <rahulanand0021@outlook.com>
2024-04-05 12:40:52 +0530

    script.py updated

--- articles/articles/script.py
+++ articles/articles/script.py
@@ -9,44 +9,17 @@ def lane(lang, articleName):
 
 # reads all articles folder, scans for all lang.json files
 # updates lang list in articles.json file
+
 def updateLangList():
-    articles = {}
-    lang = "en"
-    with open(f"articles.json", "r") as fi:
-        articlesData = json.loads(fi.read())
-
-    for article in  glob("*/"):
-        articleName = article.split("/")[0]         #get articleName
-        print(articleName)
-        articles[articleName] = {
-            "langs": [lane(lang, articleName) for lang in glob(f"{article}/*")],
-        }
-        tags = {}
-        desc = "Description Not Available"
-        tm = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-        if articleName in articlesData.keys():                         
-            tags = articlesData[articleName]["tags"]
-            desc = articlesData[articleName]["desc"]
-            tm = articlesData[articleName]["time"]
-            
-        articles[articleName]["tags"] = tags
-        articles[articleName]["desc"] = desc
-        articles[articleName]["time"] = tm
-
-    
-    try:
-        with open(f"articles.json", "w") as fo:
-            fo.write(json.dumps(articles, indent=2))
-    except:
-        print("Error writing file!")
-
-def updateLangList2():
     # characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_"
     articles = {}
+    langs = ["en", "hi"]
+    articlesLang = {lang: {} for lang in langs}
     default = {
         "en": "विवरण उपलब्ध नहीं है",
         "hi": "Description Not Available"
     }
+
     with open(f"articles.json", "r") as fi:
         articlesData = json.loads(fi.read())
     
@@ -67,10 +40,55 @@ def updateLangList2():
 
     
     try:
-        with open(f"articles2.json", "w") as fo:
+        with open(f"articles.json", "w") as fo:
             fo.write(json.dumps(articles, indent=2))
     except:
         print("Error writing file!")
+
+def updateLangList2():
+    # characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_"
+    
+    ####EXISTING DATA LOADER
+    langs = ["en", "hi"]
+    
+    with open(f"articles.json", "r") as fi:
+        articlesData = json.loads(fi.read())
+    
+    articlesLangData = {}
+    for lang in langs:
+        with open(f"articles{lang}.json", "r") as fi:
+            articlesLangData[lang] = json.loads(fi.read())
+    
+    #######################################
+
+
+    articles = {}
+    default = {
+        "en": "विवरण उपलब्ध नहीं है",
+        "hi": "Description Not Available"
+    }
+
+    
+    for article in  glob("*/"):
+        articleName = article.split("/")[0]         #get articleName
+        print(articleName)
+
+        if articleName not in articlesData.keys():
+            articlesData[articleName] = {
+                "tags" : {},
+                "langs": [],
+                "time" : datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f") 
+            }
+        articlesData[articleName]["langs"] = [lane(lang, articleName) for lang in glob(f"{article}/*")]
+        
+
+    
+    try:
+        with open(f"articles2.json", "w") as fo:
+            fo.write(json.dumps(articlesData, indent=2))
+    except:
+        print("Error writing file!")
+
 
 def main():
     updateLangList2()
