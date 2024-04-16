85e305e59d7b3c4c5ea5df79a100129d719a86fa
raccess21 <rahulanand0021@outlook.com>
2024-04-16 18:57:35 +0530

    stopwatch timer display added

--- bowl_speed.py
+++ bowl_speed.py
@@ -1,17 +1,30 @@
 import time
+import sys
+import select
 
-x, dx = [float(val.strip())/1000 for val in "20.17 +- 0.5".split("+-")]
+x, dx = [float(val.strip()) for val in "20.17 +- 0.5".split("+-")]
 
-input("Start Clock... :")
-t1 = time.time()
+input("Press Enter and Start Clock... :")
+timer = time.time()
+timed = time.time()
+while True:
+    try:
+        timed = time.time()
+        sys.stdout.write('\r')
+        sys.stdout.write(f"Press Enter and Stop  Clock... : {(timed - timer):.2f} sec")
+        sys.stdout.flush()
+        time.sleep(0.05)
+        if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
+            input("\n")
+            break
+    except KeyboardInterrupt:
+        print("\n")
+        break
 
-input("Stop  Clock... :")
-timer = time.time() - t1
-
-t, dt = [float(val.strip())/3600 for val in f"{timer} +- 0.5".split("+-")]
-v = x / t
-dv = dx / x + dt / t
-
-print(f"Distance measured: {x:08.5f} +- {dx:08.5f} kms")
-print(f"Time measured is : {t:08.5f} +- {dt:08.5f} hrs")
-print(f"Bowling speed is : {v:08.5f} +- {dv:08.5f} km/h")
+timer = timed - timer
+t, dt = [float(val.strip()) for val in f"{timer} +- 0.3".split("+-")]
+v = (x / t) * 3.6
+dv = (dx / x + dt / t) * v
+print(f"Distance measured: {x:05.2f} +- {dx:05.2f} metres_")
+print(f"Time measured is : {t:05.3f} +- {dt:03.3f} seconds")
+print(f"Bowling speed is : {v:05.2f} +- {dv:05.2f} km/h")
