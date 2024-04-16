3370ed61517b2be7609d4c553cecc241172bf9f9
raccess21 <rahulanand0021@outlook.com>
2024-04-16 13:16:02 +0530

    formatted output

--- bowl_speed.py
+++ bowl_speed.py
@@ -1,9 +1,9 @@
-x, dx = [float(val.strip()) for val in  "1 + 0.1".split("+")]
-t, dt = [float(val.strip()) for val in  "1 + 0.1".split("+")]
+x, dx = [float(val.strip())/1000 for val in  "20.17 +- 0.5".split("+-")]
+t, dt = [float(val.strip())/3600 for val in  "1 +- 0.1".split("+-")]
 
 v = x / t
 dv = dx / x + dt / t
 
-print(f"Distance measured: {x:.2f} +- {dx} metres")
-print(f"Time measured is : {t:.2f} +- {dt} seconds")
-print(f"Bowling speed is : {v:.2f} +- {dv} m / s")
+print(f"Distance measured: {x:08.5f} +- {dx:08.5f} kms")
+print(f"Time measured is : {t:08.5f} +- {dt:08.5f} hrs")
+print(f"Bowling speed is : {v:08.5f} +- {dv:08.5f} km/h")
